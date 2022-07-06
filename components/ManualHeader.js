441304c3a76a7aa993bdc2942793218ab9ea894f
Vidhan Mangla <vidhanmangla@gmail.com>
2022-07-06 16:09:19 +0100

    master

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -55,7 +55,7 @@ export default function ManualHeader() {
                             disabled={isWeb3EnableLoading}
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         >
-                            Connect
+                            Connect!
                         </button>
                     )}
                 </li>
